941795d93270eab9c6ed55b5d1fdd3ccc1a31b87
remicres <remi.cresson@teledetection.fr>
2015-08-12 19:07:18 +0200

    add mosaic.remote.cmake

---
+++ mosaic.remote.cmake
@@ -0,0 +1,6 @@
+#Contact: Rémi Cresson <remi.cresson@teledetection.fr>
+otb_fetch_module(Mosaic
+  "images mosaicking"
+  GIT_REPOSITORY https://github.com/remicres/otb-mosaic.git
+  GIT_TAG b7d42397837656c07b0b5331fc4c9a64bbbcf0d2
+)
