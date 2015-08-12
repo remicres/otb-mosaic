1628528e932efd1a4bcba8812bcdce51165454df
remicres <remi.cresson@teledetection.fr>
2015-08-12 19:38:54 +0200

    adding revision

--- mosaic.remote.cmake
+++ mosaic.remote.cmake
@@ -2,5 +2,5 @@
 otb_fetch_module(Mosaic
   "images mosaicking"
   GIT_REPOSITORY https://github.com/remicres/otb-mosaic.git
-  GIT_TAG b7d42397837656c07b0b5331fc4c9a64bbbcf0d2
+  GIT_TAG d5d8b201dcca5bd21517f0c9730d0ca2f9e6dd35
 )
