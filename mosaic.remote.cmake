4ef5d702bea5308b9dbb9b4a4dde2ea04d203806
Rémi <remi.cresson@teledetection.fr>
2016-03-11 12:41:35 +0100

    Update mosaic.remote.cmake
    Update git tag

--- mosaic.remote.cmake
+++ mosaic.remote.cmake
@@ -2,5 +2,5 @@
 otb_fetch_module(Mosaic
   "images mosaicking"
   GIT_REPOSITORY https://github.com/remicres/otb-mosaic.git
-  GIT_TAG 7e61737885f787db1a8ea7638a6a71448768e8c4
+  GIT_TAG 1406369c1ec9accffc0b3f40d0411e78bed2d893
 )
